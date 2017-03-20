6fcba00baa2d74b45fad15fe6ec9a7665c4b4645
skyhorsetomoon <15667001920@163.com>
2017-03-20 14:55:39 +0800

    Update ms_relation.R

--- R/ms_relation.R
+++ R/ms_relation.R
@@ -1,4 +1,4 @@
-ms_relation<-function(result,condition1,condition2)
+.ms_relation<-function(result, IP_BAM, Input_BAM, contrast_IP_BAM, contrast_Input_BAM, condition1, condition2)
 {
   s<-result[[1]]
   ind<-unique(s$pos)
@@ -25,34 +25,9 @@ ms_relation<-function(result,condition1,condition2)
   {
     ms_f<-function(bam)
     {
-      s1 <-bam[se]
-      for (i in 2:length(ind))
-      {
-        se <- seq(i, n, len)
-        s1 <- cbind(s1,bam[se])
-        
-      }
-      row.mean<-apply(s1,1,mean)
-      Mean<-row.mean
-      Sd<-apply(s1,1,sd)
-      IP<-cbind(Mean,Sd)
-      IP<-as.data.frame(IP)
-      z<-which(IP$Mean==0)
-      IP<-IP[-z,]
-      Mean<-log10(IP$Mean)
-      Sd<-log10(IP$Sd)
-      out<-list(Mean,Sd)
+      s1 <-.singleBAMreads(bam, se, len, n)
+      out<-.getmeanSD(s1)
       return(out)
-    }
-    f<-function(bam)
-    {
-      w<-bam[se]
-      for (i in 2:length(ind))
-      {
-        se <- seq(i, n, len)
-        w<- cbind(w, bam[se])
-      }
-      return(w)
     }
     unit_ms_f<-function(group)
     {
@@ -62,43 +37,35 @@ ms_relation<-function(result,condition1,condition2)
       for(i in 1:ncol(group))
       {
         
-        m<-f(group[,i])
+        m<-.singleBAMreads(group[,i], se, len, n)
         v<-m+v
       }
       v<-v/ncol(group)
-      Mean<-apply(v,1,mean)
-      Sd<-apply( v,1,sd)
-      Input<-cbind(Mean,Sd)
-      Input<-as.data.frame(Input)
-      z<-which(Input$Mean==0)
-      Input<-Input[-z,]
-      Mean<-log10(Input$Mean)
-      Sd<-log10(Input$Sd)
-      out<-list(Mean,Sd)
+      out<-.getmeanSD(v)
       return(out)
     }
     IPa<-data.frame()
     ID<-vector()
     group1<-as.matrix(group1)
-    for(i in 1:ncol(group1))
+    for(i in seq_len(ncol(group1)))
     {
       outa<-ms_f(group1[,i])
       Mean<-outa[[1]]
-      Sd<-outa[[2]]
-      id<-rep(group1_name[i],length(Sd))
+      SD<-outa[[2]]
+      id<-rep(group1_name[i],length(SD))
       ID<-c(ID,id)
       id<-vector()
-      IP<-cbind(Mean,Sd)
+      IP<-cbind(Mean,SD)
       IPb<-IP
       IPa<-rbind(IPa,IPb)
       IP<-matrix()
     }
     outb<-unit_ms_f(group2)
     Mean<-outb[[1]]
-    Sd<-outb[[2]]
-    Input<-cbind(Mean,Sd)
+    SD<-outb[[2]]
+    Input<-cbind(Mean,SD)
     Input<-as.data.frame(Input)
-    Id<-rep(group2_name,length(Input$Sd))
+    Id<-rep(group2_name,length(Input$SD))
     ID<-c(ID,Id)
     com<-rbind(IPa,Input)
     com<-cbind(com,ID)
@@ -106,7 +73,7 @@ ms_relation<-function(result,condition1,condition2)
     cod<-rep(condition,length(com$ID))
     com<-cbind(com,cod)
     com<-as.data.frame(com)
-    names(com)<-c("Mean","Sd","ID","Condition")
+    names(com)<-c("Mean","SD","ID","Condition")
     return(com)
   }
   if((length(reference_IP_groupname)==0)&(length(reference_Input_groupname)==0))
@@ -115,18 +82,26 @@ ms_relation<-function(result,condition1,condition2)
     Group_Input<-sa[,-(1:length(IP_groupname))]
     coma<-ms(Group_IP,Group_Input,IP_groupname,"unified_Input",paste(condition1,"condition"))
     comb<-ms(Group_Input,Group_IP,Input_groupname,"unified_IP",paste(condition1,"condition"))
+    coma<-as.data.frame(coma)
+    Mean<-coma$Mean
+    SD<-coma$SD
+    ID<-coma$ID
     m_p1<-
-      ggplot(coma,aes(Mean,Sd,colour=ID))+facet_grid(~Condition)+
+      ggplot(coma,aes(Mean,SD,colour=ID))+facet_grid(~Condition)+
       geom_smooth(aes(group = ID),span = 0.5)+
       geom_point(alpha = I(1 /150),size=0.2)+
       theme(title= element_text(size=12,color="black"))+
-      labs(title="Mean-variance relationship within IP group under one condition")
+      labs(title="Mean-SD relationship within IP group under one condition")
+    comb<-as.data.frame(comb)
+    Mean<-comb$Mean
+    SD<-comb$SD
+    ID<-comb$ID
     m_p2<-
-      ggplot(comb,aes(Mean,Sd,colour=ID))+facet_grid(~Condition)+
+      ggplot(comb,aes(Mean,SD,colour=ID))+facet_grid(~Condition)+
       geom_smooth(aes(group = ID),span = 0.5)+
       geom_point(alpha = I(1 /150),size=0.2)+
       theme(title= element_text(size=12,color="black"))+
-      labs(title="Mean-variance relationship within Input group under one condition")
+      labs(title="Mean-SD relationship within Input group under one condition")
     .multiplot(m_p1,m_p2,cols = 1)
   }
   else if((length(reference_IP_groupname)!=0)&(length(reference_Input_groupname)!=0))
@@ -149,17 +124,23 @@ ms_relation<-function(result,condition1,condition2)
     com4<-ms(ref_group_Input,ref_group_IP,reference_Input_groupname,"unified_IP",paste(condition2,"condition"))
     comb<-rbind(com3,com4)
     comb<-as.data.frame(comb)
+    Mean<-coma$Mean
+    SD<-coma$SD
+    ID<-coma$ID
     m_p1<-
-      ggplot(coma,aes(Mean,Sd,colour=ID))+facet_grid(~Condition)+
+      ggplot(coma,aes(Mean,SD,colour=ID))+facet_grid(~Condition)+
       geom_smooth(aes(group = ID),span = 0.5)+     
       geom_point(alpha = I(1 /150),size=0.2)+
       theme(title= element_text(size=12,color="black"))+
-      labs(title="Mean-variance relationship within IP group under two condition")
+      labs(title="Mean-SD relationship within IP group under two condition")
+    Mean<-comb$Mean
+    SD<-comb$SD
+    ID<-comb$ID
     m_p2<-
-      ggplot(comb,aes(Mean,Sd,colour=ID))+facet_grid(~Condition)+
+      ggplot(comb,aes(Mean,SD,colour=ID))+facet_grid(~Condition)+
       geom_smooth(aes(group = ID),span = 0.5)+
       geom_point(alpha = I(1 /150),size=0.2)+theme(title= element_text(size=12,color="black"))+
-      labs(title="Mean-variance relationship within Input group under two condition")
+      labs(title="Mean-SD relationship within Input group under two condition")
     .multiplot(m_p1,m_p2,cols = 2)
   }
 }
