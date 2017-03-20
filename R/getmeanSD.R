4e2ee645a268ccdf2a27c153f85e2fe3ab42cd4e
skyhorsetomoon <15667001920@163.com>
2017-03-20 15:03:54 +0800

    Add files via upload

---
+++ R/getmeanSD.R
@@ -0,0 +1,14 @@
+.getmeanSD<-function(matr)
+{
+  Mean<-apply(matr,1,mean)
+  SD<-apply(matr,1,sd)
+  Input<-cbind(Mean,SD)
+  Input<-as.data.frame(Input)
+  z<-which(Input$Mean==0)
+  Input<-Input[-z,]
+  Mean<-log10(Input$Mean)
+  SD<-log10(Input$SD)
+  out<-list(Mean,SD)
+  return(out)
+  
+}
