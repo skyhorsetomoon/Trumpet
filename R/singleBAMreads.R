7d128b9f91afd45f139d53519b7978e98b4f5980
skyhorsetomoon <15667001920@163.com>
2017-03-20 15:04:47 +0800

    Add files via upload

---
+++ R/singleBAMreads.R
@@ -0,0 +1,10 @@
+.singleBAMreads<-function(bam, se, len, n)
+{
+  w<-vector(mode="numeric",length = 0)
+  for (i in seq_len(len))
+  {
+    se <- seq(i, n, len)
+    w<- cbind(w, bam[se])
+  }
+  return(w)
+}
