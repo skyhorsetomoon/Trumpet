104924612ddcd4646dde4ac6fc737e247ed4fde5
skyhorsetomoon <15667001920@163.com>
2017-03-20 14:52:57 +0800

    Update get_sampleid.R

--- R/get_sampleid.R
+++ R/get_sampleid.R
@@ -1,42 +1,21 @@
-
 .get.sampleid <- function(IP_BAM,Input_BAM,contrast_IP_BAM,contrast_Input_BAM) {
-  # number of samples
-  no_ip=length(IP_BAM)
-  no_input=length(Input_BAM)
-  no_treated_ip=length(contrast_IP_BAM)
-  no_treated_input=length(contrast_Input_BAM)
-
-  if ((no_treated_input == 0)&(no_treated_ip ==0)) {
-    for(i in 1:no_ip)
-    {
-      IP_name<-paste0("IP",c(1:no_ip))
-    }
-    for(i in 1: no_input)
-    {
-      Input_name<-paste0("Input",c(1:no_input))
-    }
+  
+  if ((length(seq_along(contrast_IP_BAM)) == 0)&(length(seq_along(contrast_Input_BAM)) ==0))
+  {
+    IP_name<-paste0("IP",seq_along(IP_BAM))
+    Input_name<-paste0("Input",seq_along(Input_BAM))
     referIP_name<-NULL
     referInput_name<-NULL
-    }
+  }
   
-  if ((no_treated_input > 0)&(no_treated_ip >0) ){
-    for(i in 1:no_ip)
-    {
-      IP_name<-paste0("IP",c(1:no_ip))
-    }
-    for(i in 1:no_input)
-    {
-      Input_name<-paste0("Input",c(1:no_input))
-    }
-    for(i in 1:no_treated_ip)
-    {
-      referIP_name<-paste0("refer_IP",c(1:no_treated_ip))
-    }
-    for(i in 1: no_treated_input)
-    {
-      referInput_name<-paste0("refer_Input",c(1:no_treated_input))
-    }
-    } 
+  if ((length(seq_along(contrast_IP_BAM)) > 0)&(length(seq_along(contrast_Input_BAM)) >0) )
+  {
+    
+    IP_name<-paste0("IP",seq_along(IP_BAM))
+    Input_name<-paste0("Input",seq_along(Input_BAM))
+    referIP_name<-paste0("refer_IP",seq_along(contrast_IP_BAM))
+    referInput_name<-paste0("refer_Input",seq_along(contrast_Input_BAM))
+  } 
   sample_name<-list()
   sample_name$IP_name<-IP_name
   sample_name$Input_name<-Input_name
