6d3963acda2e8650c2aa2641c552eb323e14c92a
skyhorsetomoon <15667001920@163.com>
2017-05-29 22:31:12 +0800

    Update getmeanSD.R

--- R/getmeanSD.R
+++ R/getmeanSD.R
@@ -1,14 +1,12 @@
-.getmeanSD<-function(matr)
-{
-  Mean<-apply(matr,1,mean)
-  SD<-apply(matr,1,sd)
-  Input<-cbind(Mean,SD)
-  Input<-as.data.frame(Input)
-  z<-which(Input$Mean==0)
-  Input<-Input[-z,]
-  Mean<-log10(Input$Mean)
-  SD<-log10(Input$SD)
-  out<-list(Mean,SD)
+.getmeanSD <- function(matr) {
+  Mean <- apply(matr, 1, mean)
+  SD <- apply(matr, 1, sd)
+  Input <- cbind(Mean, SD)
+  Input <- as.data.frame(Input)
+  z <- which(Input$Mean == 0)
+  Input <- Input[-z, ]
+  Mean <- log10(Input$Mean)
+  SD <- log10(Input$SD)
+  out <- list(Mean, SD)
   return(out)
-  
 }
