e552e80d53ce11f7747fa15e90f9343713cc6f74
skyhorsetomoon <15667001920@163.com>
2017-05-29 22:34:29 +0800

    Update multiplot.R

--- R/multiplot.R
+++ R/multiplot.R
@@ -1,4 +1,4 @@
-.multiplot <- function(...,plotlist=NULL,file, cols=1, layout=NULL) {
+.multiplot <- function(..., plotlist = NULL, file, cols = 1, layout = NULL) {
   
   # Make a list from the ... arguments and plotlist
   plots <- c(list(...), plotlist)
@@ -7,14 +7,13 @@
   
   # If layout is NULL, then use 'cols' to determine layout
   if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <-t(matrix(seq(1, cols * ceiling(numPlots/cols)),
-                      ncol = cols, nrow = ceiling(numPlots/cols)))
+    # Make the panel ncol: Number of columns of plots nrow: Number of rows
+    # needed, calculated from # of cols
+    layout <- t(matrix(seq(1, cols * ceiling(numPlots/cols)), ncol = cols, 
+                       nrow = ceiling(numPlots/cols)))
   }
   
-  if (numPlots==1) {
+  if (numPlots == 1) {
     print(plots[[1]])
     
   } else {
@@ -27,11 +26,10 @@
       # Get the i,j matrix positions of the regions that contain this subplot
       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
       
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row, 
                                       layout.pos.col = matchidx$col))
     }
   }
 }
-
 
 
