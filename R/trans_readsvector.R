f1d74bf1b1e7b263b7566354e140831873b9c4d7
skyhorsetomoon <15667001920@163.com>
2017-05-29 22:49:12 +0800

    Update trans_readsvector.R

--- R/trans_readsvector.R
+++ R/trans_readsvector.R
@@ -1,12 +1,10 @@
-.trans_readsvector<-function(bam, se, len, n)
-{
-  w<-vector(mode="numeric",length = 0)
-  for (i in seq_len(len))
-  {
+.trans_readsvector <- function(bam, se, len, n) {
+  w <- vector(mode = "numeric", length = 0)
+  for (i in seq_len(len)) {
     se <- seq(i, n, len)
-    w<- cbind(w, bam[se])
+    w <- cbind(w, bam[se])
   }
-  w<-as.matrix(w)
-  w<-sapply(t(w),unlist)
+  w <- as.matrix(w)
+  w <- sapply(t(w), unlist)
   return(w)
 }
