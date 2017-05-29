b326e089a5a9f53233380f9c9957e378a0fe3651
skyhorsetomoon <15667001920@163.com>
2017-05-29 22:43:47 +0800

    Update singleBAMreads.R

--- R/singleBAMreads.R
+++ R/singleBAMreads.R
@@ -1,10 +1,8 @@
-.singleBAMreads<-function(bam, se, len, n)
-{
-  w<-vector(mode="numeric",length = 0)
-  for (i in seq_len(len))
-  {
+.singleBAMreads <- function(bam, se, len, n) {
+  w <- vector(mode = "numeric", length = 0)
+  for (i in seq_len(len)) {
     se <- seq(i, n, len)
-    w<- cbind(w, bam[se])
+    w <- cbind(w, bam[se])
   }
   return(w)
 }
