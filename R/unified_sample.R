2866267d1561a4dc14fef5b70416b706e1e867de
skyhorsetomoon <15667001920@163.com>
2017-05-29 22:03:24 +0800

    Add files via upload

---
+++ R/unified_sample.R
@@ -0,0 +1,12 @@
+.unified_sample<-function(group,se,len,n)
+{
+  m<-matrix(nrow = length(se),ncol = length(ind))
+  v<-matrix(data=0,nrow=length(se),ncol = length(ind))
+  group<-as.matrix(group)
+  for(i in seq_len(ncol(group))){
+    m<-.singleBAMreads(group[,i],se,len,n)
+    v<-m+v
+  }
+  v<-v/length(ncol(group))  
+  return(v)
+}
