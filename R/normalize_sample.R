cfd9c68cf2ad36bf6a11b40d71a51e864a2c6ac3
skyhorsetomoon <15667001920@163.com>
2017-05-30 09:55:04 +0800

    Add files via upload

---
+++ R/normalize_sample.R
@@ -0,0 +1,13 @@
+.normalize_sample<-function(s1){
+  row.sum<-rowSums(s1)
+  z<-which(row.sum>10)
+  s2<-s1[z,]
+  row.mean<-rowMeans(s2)
+  for(i in seq_len(lenth(row.mean))){
+    if(row.mean[i]<2)
+      row.mean[i]<-2
+  }
+  s3<-apply(s2,2,function(x,a)x/a, a=row.mean)
+  s3<-as.matrix(s3)
+  return(s3)
+}
