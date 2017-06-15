5699becac8b2bba66e0a1d1b26717dc53d8d0495
skyhorsetomoon <15667001920@163.com>
2017-06-15 22:18:12 +0800

    Update ctest_evluate.R

--- R/ctest_evluate.R
+++ R/ctest_evluate.R
@@ -1,3 +1,74 @@
+.ct_unit_bam <- function(group, se, len, n) {
+  
+  m <- vector()
+  group <- as.matrix(group)
+  v <- rep(0, length(tr_ve <- .trans_readsvector(group[, 1], se, 
+                                                 len, n)))
+  for (i in seq_len(ncol(group))) {
+    
+    m <- .trans_readsvector(group[, i], se, len, n)
+    v <- m + v
+    
+  }
+  v <- as.matrix(v)
+  return(v)
+}
+
+.Ct_ev <- function(bam1, bam_name, bam2, maxfold, se, len, n) {
+  if (ncol(bam1) > 1) {
+    per <- vector(mode = "numeric", length = 0)
+    percent <- vector(mode = "numeric", length = 0)
+    for (i in seq_len(length(bam_name))) {
+      bam <- .trans_readsvector(bam1[, i], se, len, n)
+      total_IP <- sum(bam)
+      total_Input <- sum(bam2)
+      for (i in seq_len(maxfold)) {
+        Ctest <- ctest(bam, bam2, total_IP, total_Input, FOLD = i, 
+                       minimal_counts_in_fdr = 10)
+        cest <- as.data.frame(Ctest)
+        ce <- cest[cest$log.fdr < log(0.05), ]
+        per[i] <- length(ce$log.fdr)/length(cest$log.fdr)
+      }
+      percent <- cbind(percent, per)
+      per <- vector(mode = "numeric", length = 0)
+    }
+    colnames(percent) <- bam_name
+    percent <- as.data.frame(percent)
+    fold <- seq_len(maxfold)
+    com <- cbind(fold, percent)
+    com <- as.data.frame(com)
+    out <- list(com, percent)
+    return(out)
+  }
+
+  if (ncol(bam1) == 1) {
+    per <- vector(mode = "numeric", length = 0)
+    percent <- vector(mode = "numeric", length = 0)
+    for (i in seq_len(length(bam_name))) {
+      bam <- .trans_readsvector(bam2[, i], se, len, n)
+      total_IP <- sum(bam1)
+      total_Input <- sum(bam)
+      per <- vector(mode = "numeric", length = 0)
+      for (i in seq_len(maxfold)) {
+        Ctest <- ctest(bam1, bam, total_IP, total_Input, FOLD = i, 
+                       minimal_counts_in_fdr = 10)
+        cest <- as.data.frame(Ctest)
+        ce <- cest[cest$log.fdr < log(0.05), ]
+        per[i] <- length(ce$log.fdr)/length(cest$log.fdr)
+      }
+      percent <- cbind(percent, per)
+      per <- vector(mode = "numeric", length = 0)
+    }
+    colnames(percent) <- bam_name
+    percent <- as.data.frame(percent)
+    fold <- seq_len(maxfold)
+    com <- cbind(fold, percent)
+    com <- as.data.frame(com)
+    out <- list(com, percent)
+    return(out)
+  }
+}
+
 .ctest_evluate <- function(result, IP_BAM, Input_BAM, contrast_IP_BAM, 
                            contrast_Input_BAM, condition1, condition2) {
   s <- result[[1]]
@@ -19,75 +90,6 @@
     reference_IP_groupname <- sample_name[[3]]
     reference_Input_groupname <- sample_name[[4]]
   }
-  ct_unit_bam <- function(group) {
-    
-    m <- vector()
-    group <- as.matrix(group)
-    v <- rep(0, length(tr_ve <- .trans_readsvector(group[, 1], se, 
-                                                   len, n)))
-    for (i in seq_len(ncol(group))) {
-      
-      m <- .trans_readsvector(group[, i], se, len, n)
-      v <- m + v
-      
-    }
-    v <- as.matrix(v)
-    return(v)
-  }
-  
-  Ct_ev <- function(bam1, bam_name, bam2, maxfold) {
-    if (ncol(bam1) > 1) {
-      per <- vector(mode = "numeric", length = 0)
-      percent <- vector(mode = "numeric", length = 0)
-      for (i in seq_len(length(bam_name))) {
-        bam <- .trans_readsvector(bam1[, i], se, len, n)
-        total_IP <- sum(bam)
-        total_Input <- sum(bam2)
-        for (i in seq_len(maxfold)) {
-          Ctest <- ctest(bam, bam2, total_IP, total_Input, FOLD = i, 
-                         minimal_counts_in_fdr = 10)
-          cest <- as.data.frame(Ctest)
-          ce <- cest[cest$log.fdr < log(0.05), ]
-          per[i] <- length(ce$log.fdr)/length(cest$log.fdr)
-        }
-        percent <- cbind(percent, per)
-        per <- vector(mode = "numeric", length = 0)
-      }
-      colnames(percent) <- bam_name
-      percent <- as.data.frame(percent)
-      fold <- seq_len(maxfold)
-      com <- cbind(fold, percent)
-      com <- as.data.frame(com)
-      out <- list(com, percent)
-      return(out)
-    }
-    if (ncol(bam1) == 1) {
-      per <- vector(mode = "numeric", length = 0)
-      percent <- vector(mode = "numeric", length = 0)
-      for (i in seq_len(length(bam_name))) {
-        bam <- .trans_readsvector(bam2[, i], se, len, n)
-        total_IP <- sum(bam1)
-        total_Input <- sum(bam)
-        per <- vector(mode = "numeric", length = 0)
-        for (i in seq_len(maxfold)) {
-          Ctest <- ctest(bam1, bam, total_IP, total_Input, FOLD = i, 
-                         minimal_counts_in_fdr = 10)
-          cest <- as.data.frame(Ctest)
-          ce <- cest[cest$log.fdr < log(0.05), ]
-          per[i] <- length(ce$log.fdr)/length(cest$log.fdr)
-        }
-        percent <- cbind(percent, per)
-        per <- vector(mode = "numeric", length = 0)
-      }
-      colnames(percent) <- bam_name
-      percent <- as.data.frame(percent)
-      fold <- seq_len(maxfold)
-      com <- cbind(fold, percent)
-      com <- as.data.frame(com)
-      out <- list(com, percent)
-      return(out)
-    }
-  }
   
   if ((length(reference_IP_groupname) == 0) & (length(reference_Input_groupname) == 
                                                0)) {
@@ -95,18 +97,18 @@
     Group_IP <- as.matrix(Group_IP)
     Group_Input <- sa[, -(seq_len(length(IP_groupname)))]
     Group_Input <- as.matrix(Group_Input)
-    Unit_Input <- ct_unit_bam(Group_Input)
-    Unit_IP <- ct_unit_bam(Group_IP)
+    Unit_Input <- .ct_unit_bam(Group_Input, se, len, n)
+    Unit_IP <- .ct_unit_bam(Group_IP, se, len, n)
     ## IP
-    out1 <- suppressWarnings(Ct_ev(Group_IP, IP_groupname, Unit_Input, 
-                                   maxfold = 10))
+    out1 <- suppressWarnings(.Ct_ev(Group_IP, IP_groupname, Unit_Input, 
+                                   maxfold = 10, se, len, n))
     com1 <- out1[[1]]
     percent1 <- out1[2]
     com1 <- melt(data = com1, id = "fold", measure.vars = c(2:(ncol(Group_IP) + 
                                                                  1)))
     ## Input
-    out2 <- suppressWarnings(Ct_ev(Unit_IP, Input_groupname, Group_Input, 
-                                   maxfold = 10))
+    out2 <- suppressWarnings(.Ct_ev(Unit_IP, Input_groupname, Group_Input, 
+                                   maxfold = 10, se, len, n))
     com2 <- out2[[1]]
     percent2 <- out2[[2]]
     com2 <- melt(data = com2, id = "fold", measure.vars = c(2:(ncol(Group_Input) + 
@@ -140,34 +142,34 @@
     ref_group_IP <- as.matrix(ref_group_IP)
     ref_group_Input <- ref_group[, -(seq_len(length(reference_Input_groupname)))]
     ref_group_Input <- as.matrix(ref_group_Input)
-    Unit_Input <- ct_unit_bam(group_Input)
-    Unit_IP <- ct_unit_bam(group_IP)
-    ref_unit_Input <- ct_unit_bam(ref_group_Input)
-    ref_unit_IP <- ct_unit_bam(ref_group_IP)
+    Unit_Input <- .ct_unit_bam(group_Input, se, len, n)
+    Unit_IP <- .ct_unit_bam(group_IP, se, len, n)
+    ref_unit_Input <- .ct_unit_bam(ref_group_Input, se, len, n)
+    ref_unit_IP <- .ct_unit_bam(ref_group_IP, se, len, n)
     ## IP
-    out1 <- suppressWarnings(Ct_ev(group_IP, IP_groupname, Unit_Input, 
-                                   maxfold = 10))
+    out1 <- suppressWarnings(.Ct_ev(group_IP, IP_groupname, Unit_Input, 
+                                   maxfold = 10, se, len, n))
     com1 <- out1[[1]]
     percent1 <- out1[[2]]
     com <- melt(data = com1, id = "fold", measure.vars = c(2:(ncol(group_IP) + 
                                                                 1)))
     ## Input
-    out2 <- suppressWarnings(Ct_ev(Unit_IP, Input_groupname, group_Input, 
-                                   maxfold = 10))
+    out2 <- suppressWarnings(.Ct_ev(Unit_IP, Input_groupname, group_Input, 
+                                   maxfold = 10, se, len, n))
     com2 <- out2[[1]]
     percent2 <- out2[[2]]
     com2 <- melt(data = com2, id = "fold", measure.vars = c(2:(ncol(group_Input) + 
                                                                  1)))
     ## refer_IP
-    refer_out1 <- suppressWarnings(Ct_ev(ref_group_IP, reference_IP_groupname, 
-                                         ref_unit_Input, maxfold = 10))
+    refer_out1 <- suppressWarnings(.Ct_ev(ref_group_IP, reference_IP_groupname, 
+                                         ref_unit_Input, maxfold = 10, se, len, n))
     refer_com1 <- refer_out1[[1]]
     refer_percent1 <- refer_out1[[2]]
     refer_com1 <- melt(data = refer_com1, id = "fold", measure.vars = c(2:(ncol(ref_group_IP) + 
                                                                              1)))
     ## refer_Input
-    refer_out2 <- suppressWarnings(Ct_ev(ref_unit_IP, reference_Input_groupname, 
-                                         ref_group_Input, maxfold = 10))
+    refer_out2 <- suppressWarnings(.Ct_ev(ref_unit_IP, reference_Input_groupname, 
+                                         ref_group_Input, maxfold = 10, se, len, n))
     refer_com2 <- refer_out2[[1]]
     refer_percent2 <- refer_out2[[2]]
     refer_com2 <- melt(data = refer_com2, id = "fold", measure.vars = c(2:(ncol(ref_group_Input) + 
