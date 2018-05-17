24e50b25f8a253515c4c8340b2d3d1d6bdfd1862
skyhorsetomoon <15667001920@163.com>
2018-05-17 11:26:18 +0800

    Update read_distribute.R

--- R/read_distribute.R
+++ R/read_distribute.R
@@ -82,49 +82,65 @@
     df2 <- as.data.frame(df2)
     
     colnames(df1)<-c("pos","Quantile","value","ID1")
-    pos <- df1$posample
+    pos <- df1$pos
     value <- df1$value
     Quantile <- df1$Quantile
+    pos_utr5 <- unique(pos)[1:20]
+    pos_cds <- unique(pos)[21:40]
+    pos_utr3 <- unique(pos)[41:60]
+    rescale_utr5 <- rescale(pos_utr5, c(1-0.14,1), from = c(0,1))
+    rescale_utr3 <- rescale(pos_utr3, to=c(2,2+0.52), from = c(2,3))
+    pos <- rep( c( rescale_utr5,  pos_cds, rescale_utr3), 3*length(unique(df1$ID1)))
+    df1 <- cbind(pos, df1[,-1])
+    
     p1 <- ggplot(df1, aes(pos, value, colour = Quantile)) + 
       geom_line() + 
-      facet_wrap(~ID1) +  
-      annotate("text", x = 0.5, y = -0.4, label = "5'UTR", size = 4) + 
-      annotate("text", x = 1.5, y = -0.4, label = "CDS", size = 4) + 
-      annotate("text", x = 2.5, y = -0.4, label = "3'UTR", size = 4) + 
-      geom_vline(xintercept = 1:2, linetype = "dotted") + 
-      annotate("rect", xmin = 0.025, xmax = 0.975, ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 1.025, xmax = 1.975, ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 2.025, xmax = 2.975, ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      theme(axis.title.x =element_text(size=12), axis.title.y=element_text(size=12),
-            title = element_text(size = 12),
+      facet_grid(ID1~.) +  
+      geom_vline(xintercept = 1:2, linetype = "dotted", colour="red") + 
+      annotate("rect", xmin = pos[1], xmax = pos[length(unique(pos))/3], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = 1.006, xmax = 1.996, ymin = -0.25, ymax = -0.05, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = pos[(length(unique(pos))*2/3+1)]-0.007, xmax = pos[length(unique(pos))], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("text", x = (pos[length(unique(pos))/6]+0.005), y= -0.4, label = "5'UTR", size = 3) + 
+      annotate("text", x = 1.5, y = -0.4, label = "CDS", size = 3) + 
+      annotate("text", x = 2.25, y = -0.4, label = "3'UTR", size = 3) + 
+      theme(axis.title.x =element_text(size=9), axis.title.y=element_text(size=9),
+            title = element_text(size = 9),
             plot.title = element_text(hjust = 0.5),
             legend.key.height=unit(0.5,'cm'),
             legend.key.width=unit(0.25,'cm'),
-            legend.text=element_text(size=10),
-            legend.title=element_text(size=10))+
-      labs(title = paste("IP and Unified Input Reads Coverage within", condition1), x = "mRNA", y = "Normalized Reads Density")
+            legend.text=element_text(size=9),
+            legend.title=element_text(size=9))+
+      labs(title = paste("IP and Unified Input Reads Coverage within", condition1), x = "mRNA", y = "Normalized Reads Density") 
     
     colnames(df2)<-c("pos","Quantile","value","ID2")
     pos <- df2$pos
     value <- df2$value
     Quantile <- df2$Quantile
+    pos_utr5 <- unique(pos)[1:20]
+    pos_cds <- unique(pos)[21:40]
+    pos_utr3 <- unique(pos)[41:60]
+    rescale_utr5 <- rescale(pos_utr5, c(1-0.14,1), from = c(0,1))
+    rescale_utr3 <- rescale(pos_utr3, to=c(2,2+0.52), from = c(2,3))
+    pos <- rep( c( rescale_utr5,  pos_cds, rescale_utr3), 3*length(unique(df2$ID2)))
+    df2 <- cbind(pos, df2[,-1])
+    
     p2 <- ggplot(df2, aes(pos, value, colour = Quantile)) +
       geom_line() + 
-      facet_wrap(~ID2) +  
-      annotate("text", x = 0.5, y = -0.4, label = "5'UTR", size = 4) + 
-      annotate("text", x = 1.5, y = -0.4, label = "CDS", size = 4) + 
-      annotate("text", x = 2.5, y = -0.4, label = "3'UTR", size = 4) + 
-      geom_vline(xintercept = 1:2, linetype = "dotted") +
-      annotate("rect", xmin = 0.025, xmax = 0.975, ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") +
-      annotate("rect", xmin = 1.025, xmax = 1.975, ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 2.025, xmax = 2.975, ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      theme(axis.title.x =element_text(size=12), axis.title.y=element_text(size=12), 
-            title = element_text(size = 12),
+      facet_grid(ID2~.) +  
+      geom_vline(xintercept = 1:2, linetype = "dotted", colour="red") + 
+      annotate("rect", xmin = pos[1], xmax = pos[length(unique(pos))/3], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = 1.006, xmax = 1.996, ymin = -0.25, ymax = -0.05, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = pos[(length(unique(pos))*2/3+1)]-0.007, xmax = pos[length(unique(pos))], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("text", x = (pos[length(unique(pos))/6]+0.005), y= -0.4, label = "5'UTR", size = 3) + 
+      annotate("text", x = 1.5, y = -0.4, label = "CDS", size = 3) + 
+      annotate("text", x = 2.25, y = -0.4, label = "3'UTR", size = 3) + 
+      theme(axis.title.x =element_text(size=9), axis.title.y=element_text(size=9),
+            title = element_text(size = 9),
             plot.title = element_text(hjust = 0.5),
             legend.key.height=unit(0.5,'cm'),
             legend.key.width=unit(0.25,'cm'),
-            legend.text=element_text(size=10),
-            legend.title=element_text(size=10))+
+            legend.text=element_text(size=9),
+            legend.title=element_text(size=9))+
       labs(title = paste("Input and Unified IP Reads Coverage within",condition1), x = "mRNA", y = "Normalized Reads Density") 
     
     .multiplot(p1, p2, cols = 1)
@@ -197,19 +213,27 @@
     Df2 <- as.data.frame(Df2)
     
     colnames(df1)<-c("pos","Quantile","value","ID1")
-    pos <- df1$posample
+    pos <- df1$pos
     value <- df1$value
     Quantile <- df1$Quantile
+    pos_utr5 <- unique(pos)[1:20]
+    pos_cds <- unique(pos)[21:40]
+    pos_utr3 <- unique(pos)[41:60]
+    rescale_utr5 <- rescale(pos_utr5, c(1-0.14,1), from = c(0,1))
+    rescale_utr3 <- rescale(pos_utr3, to=c(2,2+0.52), from = c(2,3))
+    pos <- rep( c( rescale_utr5,  pos_cds, rescale_utr3), 3*length(unique(df1$ID1)))
+    df1 <- cbind(pos, df1[,-1])
+    
     p1 <- ggplot(df1, aes(pos, value, colour = Quantile)) + 
       geom_line() + 
       facet_grid(ID1~.) +  
-      annotate("text", x = 0.5, y = -0.6, label = "5'UTR", size = 3) + 
-      annotate("text", x = 1.5, y = -0.6, label = "CDS", size = 3) + 
-      annotate("text", x = 2.5, y = -0.6, label = "3'UTR", size = 3) + 
-      geom_vline(xintercept = 1:2, linetype = "dotted") + 
-      annotate("rect", xmin = 0.025, xmax = 0.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 1.025, xmax = 1.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 2.025, xmax = 2.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      geom_vline(xintercept = 1:2, linetype = "dotted", colour="red") + 
+      annotate("rect", xmin = pos[1], xmax = pos[length(unique(pos))/3], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = 1.006, xmax = 1.996, ymin = -0.25, ymax = -0.05, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = pos[(length(unique(pos))*2/3+1)]-0.007, xmax = pos[length(unique(pos))], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("text", x = (pos[length(unique(pos))/6]+0.005), y= -0.4, label = "5'UTR", size = 3) + 
+      annotate("text", x = 1.5, y = -0.4, label = "CDS", size = 3) + 
+      annotate("text", x = 2.25, y = -0.4, label = "3'UTR", size = 3) + 
       theme(axis.title.x =element_text(size=9), axis.title.y=element_text(size=9),
             title = element_text(size = 9),
             plot.title = element_text(hjust = 0.5),
@@ -223,16 +247,24 @@
     pos <- df2$pos
     value <- df2$value
     Quantile <- df2$Quantile
+    pos_utr5 <- unique(pos)[1:20]
+    pos_cds <- unique(pos)[21:40]
+    pos_utr3 <- unique(pos)[41:60]
+    rescale_utr5 <- rescale(pos_utr5, c(1-0.14,1), from = c(0,1))
+    rescale_utr3 <- rescale(pos_utr3, to=c(2,2+0.52), from = c(2,3))
+    pos <- rep( c( rescale_utr5,  pos_cds, rescale_utr3), 3*length(unique(df2$ID2)))
+    df2 <- cbind(pos, df2[,-1])
+    
     p2 <- ggplot(df2, aes(pos, value, colour = Quantile)) +
       geom_line() + 
       facet_grid(ID2~.) +  
-      annotate("text", x = 0.5, y = -0.6, label = "5'UTR", size = 3) + 
-      annotate("text", x = 1.5, y = -0.6, label = "CDS", size = 3) + 
-      annotate("text", x = 2.5, y = -0.6, label = "3'UTR", size = 3) + 
-      geom_vline(xintercept = 1:2, linetype = "dotted") +
-      annotate("rect", xmin = 0.025, xmax = 0.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") +
-      annotate("rect", xmin = 1.025, xmax = 1.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 2.025, xmax = 2.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      geom_vline(xintercept = 1:2, linetype = "dotted", colour="red") + 
+      annotate("rect", xmin = pos[1], xmax = pos[length(unique(pos))/3], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = 1.006, xmax = 1.996, ymin = -0.25, ymax = -0.05, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = pos[(length(unique(pos))*2/3+1)]-0.007, xmax = pos[length(unique(pos))], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("text", x = (pos[length(unique(pos))/6]+0.005), y= -0.4, label = "5'UTR", size = 3) + 
+      annotate("text", x = 1.5, y = -0.4, label = "CDS", size = 3) + 
+      annotate("text", x = 2.25, y = -0.4, label = "3'UTR", size = 3) + 
       theme(axis.title.x =element_text(size=9), axis.title.y=element_text(size=9),
             title = element_text(size = 9),
             plot.title = element_text(hjust = 0.5),
@@ -242,21 +274,29 @@
             legend.title=element_text(size=9))+
       labs(title = paste("Input and Unified IP Reads Coverage within",condition1), x = "mRNA", y = "Normalized Reads Density") 
     
-    colnames(Df1)<-c("pos","Quantile","value","refer_ID1")
+    colnames(Df1)<-c("pos","Quantile","value","Refer_ID1")
     pos <- Df1$pos
     value <- Df1$value
     Quantile <- Df1$Quantile
+    pos_utr5 <- unique(pos)[1:20]
+    pos_cds <- unique(pos)[21:40]
+    pos_utr3 <- unique(pos)[41:60]
+    rescale_utr5 <- rescale(pos_utr5, c(1-0.14,1), from = c(0,1))
+    rescale_utr3 <- rescale(pos_utr3, to=c(2,2+0.52), from = c(2,3))
+    pos <- rep( c( rescale_utr5,  pos_cds, rescale_utr3), 3*length(unique(Df1$Refer_ID1)))
+    Df1 <- cbind(pos, Df1[,-1])
+    
     p3 <- ggplot(Df1, aes(pos, value, colour = Quantile)) + 
       geom_line() + 
-      facet_grid(refer_ID1~.) + 
-      annotate("text", x = 0.5, y = -0.6, label = "5'UTR", size = 3) + 
-      annotate("text", x = 1.5, y = -0.6, label = "CDS", size = 3) + 
-      annotate("text", x = 2.5, y = -0.6, label = "3'UTR", size = 3) + 
-      geom_vline(xintercept = 1:2, linetype = "dotted") +
-      annotate("rect", xmin = 0.025, xmax = 0.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") +
-      annotate("rect", xmin = 1.025, xmax = 1.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 2.025, xmax = 2.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") +
-      theme(axis.title.x =element_text(size=9), axis.title.y=element_text(size=9), 
+      facet_grid(Refer_ID1~.) + 
+      geom_vline(xintercept = 1:2, linetype = "dotted", colour="red") + 
+      annotate("rect", xmin = pos[1], xmax = pos[length(unique(pos))/3], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = 1.006, xmax = 1.996, ymin = -0.25, ymax = -0.05, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = pos[(length(unique(pos))*2/3+1)]-0.007, xmax = pos[length(unique(pos))], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("text", x = (pos[length(unique(pos))/6]+0.005), y= -0.4, label = "5'UTR", size = 3) + 
+      annotate("text", x = 1.5, y = -0.4, label = "CDS", size = 3) + 
+      annotate("text", x = 2.25, y = -0.4, label = "3'UTR", size = 3) + 
+      theme(axis.title.x =element_text(size=9), axis.title.y=element_text(size=9),
             title = element_text(size = 9),
             plot.title = element_text(hjust = 0.5),
             legend.key.height=unit(0.5,'cm'),
@@ -265,20 +305,29 @@
             legend.title=element_text(size=9))+
       labs(title = paste("Refer_IP and Unified Input Reads Coverage within", condition2),x = "mRNA", y = "Normalized Reads Density") 
     
-    colnames(Df2)<-c("pos","Quantile","value","refer_ID2")
+    colnames(Df2)<-c("pos","Quantile","value","Refer_ID2")
     pos <- Df2$pos
     value <- Df2$value
     Quantile <- Df2$Quantile
+    pos_utr5 <- unique(pos)[1:20]
+    pos_cds <- unique(pos)[21:40]
+    pos_utr3 <- unique(pos)[41:60]
+    rescale_utr5 <- rescale(pos_utr5, c(1-0.14,1), from = c(0,1))
+    rescale_utr3 <- rescale(pos_utr3, to=c(2,2+0.52), from = c(2,3))
+    pos <- rep( c( rescale_utr5,  pos_cds, rescale_utr3), 3*length(unique(Df2$Refer_ID2)))
+    Df2 <- cbind(pos, Df2[,-1])
+    
+    
     p4 <- ggplot(Df2, aes(pos, value, colour = Quantile)) +
       geom_line() + 
-      facet_grid(refer_ID2~.) +
-      annotate("text", x = 0.5, y = -0.6, label = "5'UTR", size = 3) + 
-      annotate("text", x = 1.5, y = -0.6, label = "CDS", size = 3) + 
-      annotate("text", x = 2.5, y = -0.6, label = "3'UTR", size = 3) + 
-      geom_vline(xintercept = 1:2, linetype = "dotted") + 
-      annotate("rect", xmin = 0.025, xmax = 0.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 1.025, xmax = 1.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
-      annotate("rect", xmin = 2.025, xmax = 2.975, ymin = -0.3, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      facet_grid(Refer_ID2~.) +
+      geom_vline(xintercept = 1:2, linetype = "dotted", colour="red") + 
+      annotate("rect", xmin = pos[1], xmax = pos[length(unique(pos))/3], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = 1.006, xmax = 1.996, ymin = -0.25, ymax = -0.05, alpha = 0.99, colour = "black") + 
+      annotate("rect", xmin = pos[(length(unique(pos))*2/3+1)]-0.007, xmax = pos[length(unique(pos))], ymin = -0.2, ymax = -0.1, alpha = 0.99, colour = "black") + 
+      annotate("text", x = (pos[length(unique(pos))/6]+0.005), y= -0.4, label = "5'UTR", size = 3) + 
+      annotate("text", x = 1.5, y = -0.4, label = "CDS", size = 3) + 
+      annotate("text", x = 2.25, y = -0.4, label = "3'UTR", size = 3) + 
       theme(axis.title.x =element_text(size=9), axis.title.y=element_text(size=9),
             title = element_text(size = 9),
             plot.title = element_text(hjust = 0.5),
