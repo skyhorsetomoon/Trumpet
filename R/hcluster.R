2fd1f72d6c7c802f2b10da881d11225b227e14c1
skyhorsetomoon <15667001920@163.com>
2018-05-21 13:11:43 +0800

    Update hcluster.R

--- R/hcluster.R
+++ R/hcluster.R
@@ -35,7 +35,7 @@
     reference_IP_groupname <- sample_name[[3]]
     reference_Input_groupname <- sample_name[[4]]
   }
-
+  
   
   if ((length(reference_IP_groupname) == 0) & (length(reference_Input_groupname) == 
                                                0) & ((length(IP_groupname) + length(Input_groupname)) < 3)) {
@@ -49,26 +49,132 @@
     Group_Input <- as.matrix(Group_Input)
     if (length(IP_groupname) > 2 & length(Input_groupname) > 2) {
       hct_IP <- .hc(Group_IP, IP_groupname, se, len, n)
+      ##PCA IP plot
+      phc <- prcomp(hct_IP)
+      pcd <- phc$x
+      main_pca <- pcd[,1:2]
+      main_pca <- as.data.frame(main_pca)
+      pc1 <- main_pca$PC1
+      pc2 <- main_pca$PC2
+      
+      y_fold <- max(pc2)*1
+      x_fold <- max(pc1)*1
+      if((max(pc1)>max(pc2))){
+        y_fold <- max(pc2)*2.5
+      }
+      
+      if((max(pc1)<max(pc2))){
+        
+        x_fold <- max(pc1)*2.5
+      }
+      
+      p_IP <- ggplot(main_pca, aes(PC1, PC2)) +geom_point(size=4)+ geom_text(label=rownames(main_pca),colour="red",size=4,vjust = 0, hjust = 2, angle = 270)+
+        xlim(min(pc1),x_fold )+
+        ylim(min(pc2), y_fold)
+      
+      
       hcA <- hclust(dist(hct_IP, method = "euclidean"), method = "complete")
+      ## PCA Input plot
       hct_Input <- .hc(Group_Input, Input_groupname, se, len, n)
+      phc <- prcomp(hct_Input)
+      pcd <- phc$x
+      main_pca <- pcd[,1:2]
+      main_pca <- as.data.frame(main_pca)
+      pc1 <- main_pca$PC1
+      pc2 <- main_pca$PC2
+      
+      y_fold <- max(pc2)*1
+      x_fold <- max(pc1)*1
+      if((max(pc1)>max(pc2))){
+        y_fold <- max(pc2)*2.5
+      }
+      
+      if((max(pc1)<max(pc2))){
+        
+        x_fold <- max(pc1)*2.5
+      }
+      
+      p_Input <- ggplot(main_pca, aes(PC1, PC2)) +geom_point(size=4)+ geom_text(label=rownames(main_pca),colour="red",size=4,vjust = 0, hjust = 2, angle = 270)+
+        xlim(min(pc1),x_fold )+
+        ylim(min(pc2), y_fold)
+      
       hcB <- hclust(dist(hct_Input, method = "euclidean"), method = "complete")
       nf <- layout(matrix(c(1,2),1,2,byrow = TRUE), c(1,1), c(1,1), TRUE)
       par(mar = c(5,5,1,1))
       plot(hcA, hang = -1, main = "IP group cluster")
       par(mar = c(5,5,1,1))
       plot(hcB, hang = -1, main = "Input group cluster")
+      
+      suppressWarnings(.multiplot(p_IP, p_Input,cols = 2))
     }
     if (length(IP_groupname) > 2 & length(Input_groupname) <= 2) {
       hct_IP <- .hc(Group_IP, IP_groupname, se, len, n)
       hcA <- hclust(dist(hct_IP), method = "complete")
       print("The number of Input_BAM are less three, so you can't get the Input group cluster")
       plot(hcA, hang = -1, main = "IP group cluster")
+      ## PCA IP
+      phc <- prcomp(hct_IP)
+      pcd <- phc$x
+      main_pca <- pcd[,1:2]
+      main_pca <- as.data.frame(main_pca)
+      pc1 <- main_pca$PC1
+      pc2 <- main_pca$PC2
+      
+      y_fold <- max(pc2)*1
+      x_fold <- max(pc1)*1
+      if((max(pc1)>max(pc2))){
+        y_fold <- max(pc2)*2.5
+      }
+      
+      if((max(pc1)<max(pc2))){
+        
+        x_fold <- max(pc1)*2.5
+      }
+      
+      p_IP <- ggplot(main_pca, aes(PC1, PC2)) +geom_point(size=3)+ geom_text(label=rownames(main_pca),colour="red",size=2.5,vjust = 0.3, hjust = 1.3, angle = 270)+
+        xlim(min(pc1),x_fold )+
+        ylim(min(pc2), y_fold)
+      
+      
+      
+      suppressWarnings(.multiplot(p_IP, cols = 1))
+      
+      
+      
     }
     if (length(IP_groupname) <= 2 & length(Input_groupname) > 2) {
       hct_Input <- .hc(Group_Input, Input_groupname, se, len, n)
       hcB <- hclust(dist(hct_Input), method = "complete")
       print("The number of IP_BAM are less three, so you can't get the IP group cluster")
       plot(hcB, hang = -1, main = "Input group cluster")
+      ## PCA Input
+      phc <- prcomp(hct_Input)
+      pcd <- phc$x
+      main_pca <- pcd[,1:2]
+      main_pca <- as.data.frame(main_pca)
+      pc1 <- main_pca$PC1
+      pc2 <- main_pca$PC2
+      
+      y_fold <- max(pc2)*1
+      x_fold <- max(pc1)*1
+      if((max(pc1)>max(pc2))){
+        y_fold <- max(pc2)*2.5
+      }
+      
+      if((max(pc1)<max(pc2))){
+        
+        x_fold <- max(pc1)*2.5
+      }
+      
+      p_Input <- ggplot(main_pca, aes(PC1, PC2)) +geom_point(size=3)+ geom_text(label=rownames(main_pca),colour="red",size=2.5,vjust = 0.3, hjust = 1.3, angle = 270)+
+        xlim(min(pc1),x_fold )+
+        ylim(min(pc2), y_fold)
+      
+      
+      suppressWarnings(.multiplot( p_Input, cols = 1))
+      
+      
+      
     }
   } else if ((length(reference_IP_groupname) != 0) & (length(reference_Input_groupname) != 
                                                       0)) {
@@ -101,16 +207,65 @@
       hcA <- hclust(dist(hctA), method = "complete")
       p1 <- plot(hcA, hang = -1, main = "IP group cluster")
       print("The total number of Input_BAM and contrast_Input_BAM are less three, so you can't get the Input group cluster")
-      .multiplot(p1, cols = 1)
+      ## PCA
+      phc <- prcomp(hctA)
+      pcd <- phc$x
+      main_pca <- pcd[,1:2]
+      main_pca <- as.data.frame(main_pca)
+      pc1 <- main_pca$PC1
+      pc2 <- main_pca$PC2
+      
+      y_fold <- max(pc2)*1
+      x_fold <- max(pc1)*1
+      if((max(pc1)>max(pc2))){
+        y_fold <- max(pc2)*2.5
+      }
+      
+      if((max(pc1)<max(pc2))){
+        
+        x_fold <- max(pc1)*2.5
+      }
+      
+      p_IP <- ggplot(main_pca, aes(PC1, PC2)) +geom_point(size=3)+ geom_text(label=rownames(main_pca),colour="red",size=2.5,vjust = 0.3, hjust = 1.3, angle = 270)+
+        xlim(min(pc1),x_fold )+
+        ylim(min(pc2), y_fold)
+      
+      
+      
+      suppressWarnings(.multiplot(p_IP, cols = 1))
     }
     if (((length(IP_groupname) + length(reference_IP_groupname)) < 
          3) & ((length(Input_groupname) + length(reference_Input_groupname)) > 
                2)) {
       hctB <- .hc(Input_group, group_Inputname, se, len, n)
       hcB <- hclust(dist(hctB), method = "complete")
-      p2 <- plot(hcB, hang = -1, main = "Input group cluster")
+      plot(hcB, hang = -1, main = "Input group cluster")
       print("The total number of IP_BAM and contrast_IP_BAM are less three, so you can't get the IP group cluster")
-      .multiplot(p2, cols = 1)
+      ## PCA Input
+      phc <- prcomp(hctB)
+      pcd <- phc$x
+      main_pca <- pcd[,1:2]
+      main_pca <- as.data.frame(main_pca)
+      pc1 <- main_pca$PC1
+      pc2 <- main_pca$PC2
+      
+      y_fold <- max(pc2)*1
+      x_fold <- max(pc1)*1
+      if((max(pc1)>max(pc2))){
+        y_fold <- max(pc2)*2.5
+      }
+      
+      if((max(pc1)<max(pc2))){
+        
+        x_fold <- max(pc1)*2.5
+      }
+      
+      p_Input <- ggplot(main_pca, aes(PC1, PC2)) +geom_point(size=3)+ geom_text(label=rownames(main_pca),colour="red",size=2.5,vjust = 0.3, hjust = 1.3, angle = 270)+
+        xlim(min(pc1),x_fold )+
+        ylim(min(pc2), y_fold)
+      
+      
+      suppressWarnings(.multiplot( p_Input, cols = 1))
       
     }
     if (((length(IP_groupname) + length(reference_IP_groupname)) > 
@@ -118,13 +273,65 @@
                2)) {
       hctA <- .hc(IP_group, group_IPname, se, len, n)
       hcA <- hclust(dist(hctA, method = "euclidean"), method = "complete")
+      ##PCA plot
+      phc <- prcomp(hctA)
+      pcd <- phc$x
+      main_pca <- pcd[,1:2]
+      main_pca <- as.data.frame(main_pca)
+      pc1 <- main_pca$PC1
+      pc2 <- main_pca$PC2
+      y_fold <- max(pc2)*1
+      x_fold <- max(pc1)*1
+      if((max(pc1)>max(pc2))){
+        y_fold <- max(pc2)*2.5
+      }
+      
+      if((max(pc1)<max(pc2))){
+        
+        x_fold <- max(pc1)*2.5
+      }
+      
+      
+      p_IP <- ggplot(main_pca, aes(PC1, PC2)) +geom_point(size=3)+ geom_text(label=rownames(main_pca),colour="red",size=2.5,vjust = 0.3, hjust = 1.3, angle = 270)+
+        xlim(min(pc1),x_fold )+
+        ylim(min(pc2), y_fold)
+      
+      
+      
       hctB <- .hc(Input_group, group_Inputname, se, len, n)
       hcB <- hclust(dist(hctB, method = "euclidean"), method = "complete")
+      ##PCA plot
+      phc <- prcomp(hctB)
+      pcd <- phc$x
+      main_pca <- pcd[,1:2]
+      main_pca <- as.data.frame(main_pca)
+      pc1 <- main_pca$PC1
+      pc2 <- main_pca$PC2
+      
+      y_fold <- max(pc2)*1
+      x_fold <- max(pc1)*1
+      if((max(pc1)>max(pc2))){
+        y_fold <- max(pc2)*2.5
+      }
+      
+      if((max(pc1)<max(pc2))){
+        
+        x_fold <- max(pc1)*2.5
+      }
+      
+      p_Input <- ggplot(main_pca, aes(PC1, PC2)) +geom_point(size=3)+ geom_text(label=rownames(main_pca),colour="red",size=2.5,vjust = 0.3, hjust = 1.3, angle = 270)+
+        xlim(min(pc1),x_fold )+
+        ylim(min(pc2), y_fold)
+      
+      
       nf <- layout(matrix(c(1,2),1,2,byrow = TRUE), c(1,1), c(1,1), TRUE)
       par(mar = c(5,5,1,1))
       plot(hcA, hang = -1, main = "IP group cluster")
       par(mar = c(5,5,1,1))
       plot(hcB, hang = -1, main = "Input group cluster")
+      suppressWarnings(.multiplot(p_IP, p_Input,cols = 2))
+      
+      
     }
     
   }
